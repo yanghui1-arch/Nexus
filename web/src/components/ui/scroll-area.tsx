46165871ea2cf1f4c70dbff5f5aec6e3aab4dcbb
Nexus-Sophie <appleneoplus@gmail.com>
2026-03-30 16:23:42 +0800

    Add ScrollArea UI component

---
+++ web/src/components/ui/scroll-area.tsx
@@ -0,0 +1,52 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+function ScrollArea({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="scroll-area"
+      className={cn("relative overflow-hidden", className)}
+      {...props}
+    >
+      <div
+        data-slot="scroll-area-viewport"
+        className="h-full w-full rounded-[inherit] overflow-auto"
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+function ScrollBar({
+  className,
+  orientation = "vertical",
+  ...props
+}: React.ComponentProps<"div"> & {
+  orientation?: "vertical" | "horizontal"
+}) {
+  return (
+    <div
+      data-slot="scroll-bar"
+      data-orientation={orientation}
+      className={cn(
+        "flex touch-none transition-colors select-none",
+        orientation === "vertical" &&
+          "h-full w-2.5 border-l border-l-transparent p-[1px]",
+        orientation === "horizontal" &&
+          "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+        className
+      )}
+      {...props}
+    >
+      <div className="bg-border relative flex-1 rounded-full" />
+    </div>
+  )
+}
+
+export { ScrollArea, ScrollBar }
