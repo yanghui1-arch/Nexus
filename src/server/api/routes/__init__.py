fdc393b66e8f4f2123532733effdfed97daf2653
Nexus-Tela <dasss90ovo@gmail.com>
2026-05-14 09:05:53 +0000

    feat(account): add overview route

--- src/server/api/routes/__init__.py
+++ src/server/api/routes/__init__.py
@@ -1,7 +1,8 @@
 """API route handlers."""
 
+from src.server.api.routes.account import router as account_router
 from src.server.api.routes.agent_instances import router as agent_instances_router
 from src.server.api.routes.product import router as product_router
 from src.server.api.routes.tasks import router as tasks_router
 
-__all__ = ["agent_instances_router", "product_router", "tasks_router"]
+__all__ = ["account_router", "agent_instances_router", "product_router", "tasks_router"]
